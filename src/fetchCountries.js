833b43ece891d30085f894289bfc27e6b86d4af5
SerhioAndros <androsiuksv@gmail.com>
2021-05-20 02:44:39 +0300

    Finalization of task

--- src/fetchCountries.js
+++ src/fetchCountries.js
@@ -1,6 +1,10 @@
 const BASE_URL = 'https://restcountries.eu/rest/v2/name/';
 
 const fetchCountries = (request = '') =>
-  fetch(BASE_URL + request).then(response => response.json());
-
+  fetch(BASE_URL + request).then(res => {
+    if (res.status === 404) {
+      throw new Error(`Страна не найдена! Попробуйте еще раз`);
+    }
+    return res.json();
+  });
 export { fetchCountries };
--- src/index.js
+++ src/index.js
@@ -1,10 +1,31 @@
 import './sass/main.scss';
 import { fetchCountries } from './fetchCountries.js';
-const inputNode = document.querySelector('.input-field');
+import makeCountryMarkup from './templates/countryOutput.hbs';
+import makeCountriesListMarkup from './templates/countriesList.hbs';
+import makeAlert from './templates/alert.hbs';
 
-inputNode.addEventListener('input', _.debounce(dataRequest, 500));
+const inputNode = document.querySelector('.input-field');
+const wrapperNode = document.querySelector('.output-wrapper');
+
+inputNode.addEventListener('input', _.debounce(dataRequest, 1000));
 
 function dataRequest() {
-  const countriesTmp = fetchCountries(inputNode.value);
-  console.log(countriesTmp);
+  if (inputNode.value === '') return false;
+  fetchCountries(inputNode.value).then(data => countryListHandler(data));
+}
+
+function renderCountry(data) {
+  const countryMarkup = makeCountryMarkup(...data);
+  wrapperNode.innerHTML = countryMarkup;
+}
+
+function renderCountryList(data) {
+  const countryListMarkup = data.map(country => makeCountriesListMarkup(country)).join('');
+  wrapperNode.innerHTML = `<ul class="countries-list"> ${countryListMarkup} </ul>`;
+}
+
+function countryListHandler(data) {
+  if (data.length > 10) wrapperNode.innerHTML = makeAlert();
+  if (data.length > 1 && data.length <= 10) renderCountryList(data);
+  if (data.length === 1) renderCountry(data);
 }
